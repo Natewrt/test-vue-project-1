92f42ff69e7313b6f807cb5f2331947fc1dc001c
Natewrt <nathamon.nt109@gmail.com>
2017-07-25 21:46:41 +0700

    first commit

---
+++ src/components/Profile.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <img src="../assets/logo.png" @click="onClick">
+      <h1>
+          {{msg}} - {{num}}
+      </h1>
+      <button @click="onPlusNumber">PLUS</button>
+      <button @click="onMinusNumber">MINUS</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+  data () {
+    return {
+      msg: 'Jarn jai Yen',
+      num: 1
+    }
+  },
+  methods: {
+    onClick () {
+      alert('Jarn Jai Yen')
+    },
+    onPlusNumber () {
+      this.num = this.num + 1
+      this.alertfunction()
+    },
+    onMinusNumber () {
+      this.num = this.num - 1
+      this.alertfunction()
+    },
+    alertfunction () {
+      if (this.num === 10 || this.num === -10) {
+        alert('HELLO')
+      }
+    }
+  }
+}
+</script>
